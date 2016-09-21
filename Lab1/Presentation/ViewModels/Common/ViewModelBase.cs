60409d2330fcbb48f9bc60543cc21e8229d13e76
Tetiana <Tetiana@DESKTOP-9E15VF4>
2016-09-21 23:52:29 +0300

    Fix errors

--- Lab1/Presentation/ViewModels/Common/ViewModelBase.cs
+++ Lab1/Presentation/ViewModels/Common/ViewModelBase.cs
@@ -6,19 +6,26 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Views;
 using Microsoft.Practices.ServiceLocation;
+using GalaSoft.MvvmLight.Command;
 
 namespace Lab1.Presentation.ViewModels.Common
 {
     public class ViewModelBase : GalaSoft.MvvmLight.ViewModelBase
     {
+        protected readonly ICommand _goBackCommand;
+
         public ViewModelBase()
         {
             NavigationService = ServiceLocator.Current.GetInstance<INavigationService>();
             DialogService = new DialogService();
+
+            _goBackCommand = new RelayCommand(NavigationService.GoBack);
         }
 
         protected INavigationService NavigationService { get; }
 
         protected IDialogService DialogService { get; }
+
+        public ICommand GoBackCommand => _goBackCommand;        
     }
 }
